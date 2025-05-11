321869883381e4a4ef463a2ee8e7b7f17ad79c0e
somesoo <jakasperczak@gmail.com>
2025-05-11 21:11:32 +0200

    correct regex pattern file

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from data_loader import load_dna_with_window
-from regex_generator import generate_kgram_regexes
+from regex_generator import save_kgram_regexes_to_file, load_regex_patterns
 from model_trainer import extract_features, train_and_save_model
 
 if __name__ == "__main__":
@@ -9,7 +9,8 @@ if __name__ == "__main__":
 #    all_examples = donor_examples + acceptor_examples
 
     # Generuj regexy długości 3
-    regex_list = generate_kgram_regexes(3)
+    save_kgram_regexes_to_file(3, "regex_patterns.txt")
+    regex_list = load_regex_patterns("regex_patterns.txt")
 
     # Pozycje testowe
     positions = [7, 68]  # zgodnie z poleceniem
